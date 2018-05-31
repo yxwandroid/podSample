79b2453a6f2b080b4d7d433ec9210c169a26a66c
wilson <yxw_android@163.com>
2018-05-31 11:48:02 +0800

    Release 0.0.1

---
+++ podSample/Utils.swift
@@ -0,0 +1,14 @@
+//
+//  Utils.swift
+//  podSample
+//
+//  Created by 杨学武 on 2018/5/31.
+//  Copyright © 2018年 leedian. All rights reserved.
+//
+class Utils {
+    
+    func getName() -> String {
+        return "wilson"
+    }
+
+}
--- podSample/ViewController.swift
+++ podSample/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let my = Utils()
+    
+       let name = my.getName()
+       print(name)
     }
 
     override func didReceiveMemoryWarning() {
